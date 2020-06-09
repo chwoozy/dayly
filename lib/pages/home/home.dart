ecf296a5fe34e0100af14b0477b22ea5f49e362f
yungweezy <choonweiw@gmail.com>
2020-06-10 03:01:35 +0800

    removed old home appbar

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -21,20 +21,20 @@ class Home extends StatelessWidget {
       value: DatabaseService(uid: user.uid).userData,
       child: Scaffold(
         backgroundColor: primaryBackgroundColor,
-        appBar: AppBar(
-          title: Text('Home'),
-          backgroundColor: primaryPurple,
-          elevation: 0.0,
-          actions: <Widget>[
-            IconButton(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              icon: Icon(Icons.exit_to_app),
-              onPressed: () async {
-                await _authService.signOut();
-              },
-            )
-          ],
-        ),
+        // appBar: AppBar(
+        //   title: Text('Home'),
+        //   backgroundColor: primaryPurple,
+        //   elevation: 0.0,
+        //   actions: <Widget>[
+        //     IconButton(
+        //       padding: EdgeInsets.symmetric(horizontal: 20.0),
+        //       icon: Icon(Icons.exit_to_app),
+        //       onPressed: () async {
+        //         await _authService.signOut();
+        //       },
+        //     )
+        //   ],
+        // ),
         body: Profile(),
         bottomNavigationBar: CurvedNavigationBar(
           height: 55,
