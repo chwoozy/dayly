3b6c88994b6a3169cca19e479510c57d04e97af1
yungweezy <choonweiw@gmail.com>
2020-06-24 21:42:44 +0800

    recommit landing changes

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -11,7 +11,6 @@ class AuthService {
   final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: <String>[
     CalendarApi.CalendarScope,
   ]);
-  Map<String, String> _authHeaders;
 
   // User Object from FirebaseUser
   User _userFromFirebaseUser(FirebaseUser user) {
@@ -34,7 +33,7 @@ class AuthService {
     final headers = await googleSignInAccount.authHeaders;
     final httpClient = GoogleHttpClient(headers);
     var calendar = CalendarApi(httpClient);
-    DateTime firstDayOfMonth = DateTime.;
+    DateTime firstDayOfMonth = Utils.firstDayOfMonth(DateTime.now());
     DateTime lastDayOfMonth = Utils.lastDayOfMonth(DateTime.now());
     var calEvents = calendar.events.list("primary",
         timeMax: lastDayOfMonth.toUtc(), timeMin: firstDayOfMonth.toUtc());
@@ -71,10 +70,6 @@ class AuthService {
           .updateUserData(user.email, user.displayName, user.photoUrl);
       print("First-time sign up with Google success!");
     }
-
-    print("Getting Auth Headers...");
-    _authHeaders = await googleSignInAccount.authHeaders;
-    print("Retrieved Auth Headers!");
 
     return 'Sign in with google succeeded: $user';
   }
